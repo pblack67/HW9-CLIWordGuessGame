ab08f297734f93f79bfc18291c10cb9be0d9493b
Paul Black <pblack67@comcast.net>
2019-05-20 14:17:03 -0500

    Fixed a typo

--- letter.js
+++ letter.js
@@ -1,17 +1,17 @@
 function Letter(letter) {
-    this.underyingLetter = letter;
+    this.underlyingLetter = letter;
     this.displayLetter = "_";
     this.isGuessed = (letter === " ");
     this.toString = function () {
         if (this.isGuessed) {
-            return this.underyingLetter;
+            return this.underlyingLetter;
         } else {
             return this.displayLetter;
         }
     }
     this.guessLetter = function (letter) {
         if (!this.isGuessed) {
-            if (letter.toLowerCase() === this.underyingLetter.toLowerCase()) {
+            if (letter.toLowerCase() === this.underlyingLetter.toLowerCase()) {
                 this.isGuessed = true;
                 return true;
             }
